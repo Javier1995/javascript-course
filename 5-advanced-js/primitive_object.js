310b5cf57fbff4346a9474bfd8010c5cc216324b
Javier Quiroga Almeida <jquirogaa@hmujer.saludtab.gob.mx>
2020-03-06 10:05:25 -0600

    Features: This is the primitive things

--- 5-advanced-js/primitive_object.js
+++ 5-advanced-js/primitive_object.js
@@ -1,15 +1,26 @@
-var a = 10;
+/* var a = 10;
 var b = a;
 a = 40;
+
+var array1 = [1,2,3,4];
+var array2 = array1;
+array1[0] = 'vete a la verga';
+
+
+
+console.table(array1);
+console.table(array2);
+
+
+
 
 console.log(a);
 console.log(b);
 
-
 var obj1 = {
     name: 'john',
     age: 26
-};
+}
 
 var obj2  = obj1;
 obj1.age = 30;
@@ -29,4 +40,25 @@ function change(a, b){
 
 change(age, obj);
 console.info(age);
-console.log(obj.city);
+console.log(obj.city); */
+
+/* var puta = {
+    nombre:'diamantina',
+    ets: true,
+    edad: 40
+};
+var clonaPuta = Array.a
+
+clonaPuta.ets = false;
+puta.age = 18;
+console.table(puta);
+console.table(clonaPuta);
+ */
+
+var arrayPrimero = [2,3,4,5]
+var arraySegundo = arrayPrimero.slice();
+
+arrayPrimero[1] = 123;
+
+console.log(arrayPrimero);
+console.log(arraySegundo);
